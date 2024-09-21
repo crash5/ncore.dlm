ca018567d9bccb1b5eb8f7ae1dc1031d1f5f44d1
Imre NAGY <crash@veluna.eu>
2024-09-21 17:25:37 +0200

    Setup pipeline for release

--- tools/create_package.sh
+++ tools/create_package.sh
@@ -1,3 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -e
 
-tar -czf ncore.dlm -C src ./
+readonly RELEASE_VERSION=${1:-"v0.0.0"}
+readonly OUTPUT_PATH=${2:-"output/"}
+
+readonly RELEASE_FILE_NAME=ncore_${RELEASE_VERSION}.dlm
+readonly TEMPORARY_WORK_DIR=$(mktemp -d -p .)
+
+export RELEASE_VERSION
+tools/mo src/INFO.mustache > "${TEMPORARY_WORK_DIR}"/INFO
+cp src/search.php "${TEMPORARY_WORK_DIR}"/
+
+mkdir -p "${OUTPUT_PATH}"
+tar -czf "${OUTPUT_PATH}"/${RELEASE_FILE_NAME} -C "${TEMPORARY_WORK_DIR}" ./
+
+rm -rf "${TEMPORARY_WORK_DIR}"
